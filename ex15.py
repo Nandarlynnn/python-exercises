976c94fd0753912880b524c8db071d32cb984c52
Nandarlynnn <nandarlin250@gmail.com>
2018-06-06 16:35:42 +0530

    Finish

---
+++ ex15.py
@@ -0,0 +1,9 @@
+from sys import argv
+script,filename=argv
+txt=open(ex15.py)
+print("Here's your file %r:" %ex15.pt)
+print(txt.read())
+print("Type the filename again:")
+file_again=ex15.sample(">")
+txt_again=open(file_again)
+print(txt_again.read())
