b3ca7c02b21b430494ab3dfbfff0d6963adc8148
Nandarlynnn <nandarlin250@gmail.com>
2018-06-06 08:56:19 +0530

    Hero

---
+++ ex12.py
@@ -0,0 +1,4 @@
+age=23("How old are you?")
+height=raw_input("How tall are you?")
+weight=raw_input("How much do you weight?")
+print("So , you're %r old,%r tall and %r heavy."%(age, height,weight))
