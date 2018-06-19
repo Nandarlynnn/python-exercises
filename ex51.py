8386cdc867fecc0c62a533ff78bbf673c3ca0994
Nandarlynnn <nandarlin250@gmail.com>
2018-06-19 18:36:22 +0530

    HH

--- ex51.py
+++ ex51.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app =Flask(__name__)
-@app.route("/")
+@app.route("/hello")
 def index():
+    name=request.args.get('name','Nobody')
+    if    name:
+        greeting=f"Hello, {name}"
+    else:
+        greeting ="Hello World"
     greeting="Hello World"
     return render_templae("index.html",greeting=greeting)
     if    __name__ == "__main__":
+        app.run()
     
