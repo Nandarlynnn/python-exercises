33ed70006b792b517b15a14d78aa4916e26d5dca
Nandarlynnn <nandarlin250@gmail.com>
2018-06-26 12:02:45 +0530

    Do you know this change

---
+++ ex101.py
@@ -0,0 +1,13 @@
+tabbed_dog = "\tI'm tabbed in."
+persian_dog = "I'm split\non a line."
+backslash_dog = "I'm \\ a \\ cat."
+fat_dog ="""
+I'll do a list:
+    \t* Dog food
+    \t* Fishies
+    \t* Catnip\n\t* Grass
+    """
+print("tabbed_dog")
+print("persian_dog")
+print("backslash_dog")
+print("fat_dog")
